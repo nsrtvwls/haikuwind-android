ee3b56b4adc8d2615e58e138f2feeba0594b8168
oakjumper <oakjumper@gmail.com>
2011-02-03 23:12:40 +0300

    Initial commit
    Android Eclipse project with background described only. Background picture: leaf.
    
    Signed-off-by: oakjumper <oakjumper@gmail.com>

---
+++ src/com/haikuwind/HaikuWind.java
@@ -0,0 +1,13 @@
+package com.haikuwind;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HaikuWind extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
