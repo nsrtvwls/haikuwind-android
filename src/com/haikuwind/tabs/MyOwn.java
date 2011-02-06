786962f98bf561bc3dc34fe6f2924c0b734810c1
oakjumper <oakjumper@gmail.com>
2011-02-06 23:32:30 +0300

    Added classes for Tab Activities
    removed *.class, added .gitignore

---
+++ src/com/haikuwind/tabs/MyOwn.java
@@ -0,0 +1,15 @@
+package com.haikuwind.tabs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.haikuwind.R;
+
+public class MyOwn extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.haiku_list);
+    }
+}
