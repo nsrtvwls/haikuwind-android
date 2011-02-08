1e161c018cf5bb6441d20b4841d6407ee6602f1e
oakjumper <oakjumper@gmail.com>
2011-02-09 00:00:09 +0300

    added border for haiku widget

--- gen/com/haikuwind/R.java
+++ gen/com/haikuwind/R.java
@@ -10,27 +10,32 @@ package com.haikuwind;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int transparent=0x7f040001;
+        public static final int white=0x7f040000;
+    }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int ic_tab_favorites=0x7f020001;
-        public static final int ic_tab_favorites_selected=0x7f020002;
-        public static final int ic_tab_favorites_unselected=0x7f020003;
-        public static final int ic_tab_halloffame=0x7f020004;
-        public static final int ic_tab_halloffame_selected=0x7f020005;
-        public static final int ic_tab_halloffame_unselected=0x7f020006;
-        public static final int ic_tab_myown=0x7f020007;
-        public static final int ic_tab_myown_selected=0x7f020008;
-        public static final int ic_tab_myown_unselected=0x7f020009;
-        public static final int ic_tab_timeline=0x7f02000a;
-        public static final int ic_tab_timeline_selected=0x7f02000b;
-        public static final int ic_tab_timeline_unselected=0x7f02000c;
-        public static final int ic_tab_top=0x7f02000d;
-        public static final int ic_tab_top_selected=0x7f02000e;
-        public static final int ic_tab_top_unselected=0x7f02000f;
-        public static final int icon=0x7f020010;
+        public static final int haiku_border=0x7f020001;
+        public static final int ic_tab_favorites=0x7f020002;
+        public static final int ic_tab_favorites_selected=0x7f020003;
+        public static final int ic_tab_favorites_unselected=0x7f020004;
+        public static final int ic_tab_halloffame=0x7f020005;
+        public static final int ic_tab_halloffame_selected=0x7f020006;
+        public static final int ic_tab_halloffame_unselected=0x7f020007;
+        public static final int ic_tab_myown=0x7f020008;
+        public static final int ic_tab_myown_selected=0x7f020009;
+        public static final int ic_tab_myown_unselected=0x7f02000a;
+        public static final int ic_tab_timeline=0x7f02000b;
+        public static final int ic_tab_timeline_selected=0x7f02000c;
+        public static final int ic_tab_timeline_unselected=0x7f02000d;
+        public static final int ic_tab_top=0x7f02000e;
+        public static final int ic_tab_top_selected=0x7f02000f;
+        public static final int ic_tab_top_unselected=0x7f020010;
+        public static final int icon=0x7f020011;
     }
     public static final class id {
-        public static final int haiku_list=0x7f060000;
+        public static final int haiku_list=0x7f070000;
     }
     public static final class layout {
         public static final int haiku=0x7f030000;
@@ -38,15 +43,15 @@ public final class R {
         public static final int main=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int favorites=0x7f040005;
-        public static final int hall_of_fame=0x7f040003;
-        public static final int my_own=0x7f040004;
-        public static final int timeline=0x7f040001;
-        public static final int top_chart=0x7f040002;
+        public static final int app_name=0x7f050000;
+        public static final int favorites=0x7f050005;
+        public static final int hall_of_fame=0x7f050003;
+        public static final int my_own=0x7f050004;
+        public static final int timeline=0x7f050001;
+        public static final int top_chart=0x7f050002;
     }
     public static final class style {
-        public static final int Leave=0x7f050000;
-        public static final int TabHeader=0x7f050001;
+        public static final int Leave=0x7f060000;
+        public static final int TabHeader=0x7f060001;
     }
 }
