d6ccf9ba57e8228cdaff7b53878b2c430b934279
oakjumper <oakjumper@gmail.com>
2011-02-11 23:55:11 +0300

    beginning of http requests

--- src/com/haikuwind/HaikuWind.java
+++ src/com/haikuwind/HaikuWind.java
@@ -1,9 +1,11 @@
 package com.haikuwind;
 
 import android.app.TabActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.widget.RelativeLayout;
 import android.widget.TabHost;
 import android.widget.TabWidget;
@@ -16,10 +18,24 @@ import com.haikuwind.tabs.Timeline;
 import com.haikuwind.tabs.TopChart;
 
 public class HaikuWind extends TabActivity {
-    /** Called when the activity is first created. */
+	private String userId;
+	
+	public String getUserId() {
+		return userId;
+	}
+	
+	private void initUserId() {
+		TelephonyManager tManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+		userId = tManager.getDeviceId();
+	}
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        initUserId();
+        
         setContentView(R.layout.main);
 
         Resources res = getResources(); // Resource object to get Drawables
@@ -74,4 +90,5 @@ public class HaikuWind extends TabActivity {
             tv.setTextSize(11.0f); // just example
         }     
     }
+    
 }
