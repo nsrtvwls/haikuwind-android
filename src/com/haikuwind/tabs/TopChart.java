a32bf384bcc6b6e9e758d6c72a55879f9f37bafb
oakjumper <oakjumper@gmail.com>
2011-05-26 21:36:43 +0400

    listen VOTE events, update only if haiku goes to top

--- src/com/haikuwind/tabs/TopChart.java
+++ src/com/haikuwind/tabs/TopChart.java
@@ -7,10 +7,24 @@ import com.haikuwind.feed.Haiku;
 import com.haikuwind.feed.HaikuListData;
 import com.haikuwind.feed.HaikuWindData;
 import com.haikuwind.feed.HttpRequest;
+import com.haikuwind.notification.Update;
+import com.haikuwind.notification.UpdateNotifier;
 import com.haikuwind.tabs.buttons.HasFavoriteBtn;
 import com.haikuwind.tabs.buttons.HasVoteBtn;
 
 public class TopChart extends HaikuListActivity implements HasVoteBtn, HasFavoriteBtn {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        UpdateNotifier.addUpdateListener(this, Update.VOTE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        UpdateNotifier.removeUpdateListener(this);
+    }
 
     @Override
     protected List<Haiku> fetchElements() throws FeedException {
@@ -20,5 +34,21 @@ public class TopChart extends HaikuListActivity implements HasVoteBtn, HasFavori
     @Override
     protected HaikuListData getHaikuListData() {
         return HaikuWindData.getInstance().getTopChartData();
+    }
+    
+    @Override
+    public void processUpdate(Update update, Haiku haiku) {
+        if(Update.VOTE==update) {
+            List<Haiku> topChartList = getHaikuListData().getHaikuList();
+            if(topChartList.size()>0) {
+                Haiku minPointsHaiku = topChartList.get(topChartList.size()-1);
+                if(minPointsHaiku.getPoints()>haiku.getPoints()) {
+                    //voted haiku doesn't affect top chart, skip update
+                    return;
+                }
+            }
+        }
+        
+        super.processUpdate(update, haiku);
     }
 }
